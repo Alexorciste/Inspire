707eafb70cc4dab5dfc07bca55d50c1f3cd5133a
julien daniel <j.delarita@gmail.com>
2020-11-30 18:34:10 +0100

    Install_Pundit_Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,57 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
+    @projects = policy_scope(Project)
   end
 
   def show
   end
 
   def new
+    @project = Project.new
+    authorize @project
   end
 
   def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+    authorize @project
+    if @project.save
+      redirect_to @project, notice: 'Project was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+     if @project.update(project_params)
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @project.destroy
+    redirect_to projects_url, notice: 'Project was successfully destroyed.'
+
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+    authorize @project
+  end
+
+  def project_params
+    params.require(:project).permit(:title)
   end
 end
+
+
+

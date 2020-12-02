70f940e97a298cac2488cab37683dedb44dabaee
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2020-12-02 15:04:00 +0100

    versionok

--- app/javascript/components/opendiv.js
+++ app/javascript/components/opendiv.js
@@ -1,15 +1,14 @@
 const openNav = () => {
-    
+   
     const redButton = document.querySelector(".btn-danger");
     const divBox = document.querySelector(".box");
-    
     redButton.addEventListener("click", (event) => {
       divBox.classList.toggle("active");
       
-
       
     });
-      
+    
+  
   };
 
 export { openNav };
--- app/javascript/components/wordselection.js
+++ app/javascript/components/wordselection.js
@@ -2,6 +2,7 @@ const wordSelector = () => {
     const divText = document.querySelector("#text_content")
     console.log(divText);
     divText.addEventListener("keyup", (event) => {
+        if(event.key=" ")
         console.log(event);
     })
     

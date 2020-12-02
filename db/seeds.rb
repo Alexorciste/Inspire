42f9ddf88a064637015e08e8d6471ea0acebaf53
julien daniel <j.delarita@gmail.com>
2020-12-02 14:04:43 +0100

    Select2 and Jqueary Install_Pas certain que cela fonctione come voulu

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Project.destroy_all
+# User.destroy_all
+
+# user = User.new(email: "polo1@polo.fr", password: "azerty" )
+# user.save!
+# user = User.new(email: "polo2@polo.fr", password: "azerty" )
+# user.save!
+# user = User.new(email: "polo3@polo.fr", password: "azerty" )
+# user.save!
+# user = User.new(email: "polo4@polo.fr", password: "azerty" )
+# user.save!
+# user = User.new(email: "polo5@polo.fr", password: "azerty" )
+# user.save!
+# user = User.new(email: "polo6@polo.fr", password: "azerty" )
+# user.save!
+# user = User.new(email: "polo7@polo.fr", password: "azerty" )
+# user.save!
+# user = User.new(email: "polo8@polo.fr", password: "azerty" )
+# user.save!
+# user = User.new(email: "polo9@polo.fr", password: "azerty" )
+# user.save!

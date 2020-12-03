13da99b057b59c72c00d1e2892190e58f869db5a
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2020-12-03 17:50:53 +0100

    jswordselector

--- app/javascript/components/wordselection.js
+++ app/javascript/components/wordselection.js
@@ -1,25 +1,51 @@
 
 
 const wordSelector = () => {
+    let listSpan = [];
+    const urlcourante = document.location.href;
+    // console.log(testString(urlcourante));
+    //regex sur http://localhost:3000/texts/14/edit
+    const listenKey = document.querySelector("#text_content");
     
-    const keyText = document.querySelector("#text_content")
-    
-    keyText.addEventListener("keyup", (event) => {
+    listenKey.addEventListener("keyup", (event) => {
         
-        // if(event.key == " ");
-        const divText = document.querySelector(".form-inputs");
-        const val = divText.textContent; 
-        const split = val.split(/[\s,\n]+/);
-        console.log(split);
-        console.log(event);
+        const test = document.querySelector("#test");
+        const divText = document.querySelector("#text_content > div");
+        const splitText = divText.innerText.split(/[\s,;:']+/);
         
 
-    })
-    
+        
+        
+                
+        test.innerHTML = " ";
+        const array = []
+        splitText.forEach((word, index) => {
+            array.push(`<span>${word}</span>`) 
+            test.insertAdjacentHTML("beforeend", `<span id="span${index}">${word} </span>` )
+
+            
+                // console.log(splitText);
+        });
+        
+            test.querySelectorAll("span").forEach((span) => {
+            console.log(span);
+            span.addEventListener("click", (event) => {
+            console.log(event);
+                
+            });
+        });  
+       
+
+      const finalString = array.join()
+      document.querySelector("#text_content_trix_input_text_17").value = `<div>${finalString}</div>`
+
+    });
+
+   
 };
 
 
 
-
-
 export { wordSelector };
+
+
